c0fcdc56c62e8cb85083cf684bad8fb868c51c28
dttg-nguyen <dttg-nguyen@users.noreply.github.com>
2020-10-27 23:45:45 -0400

    Update forms, add partials and update js files
    Polish js and html

--- public/js/index.js
+++ public/js/index.js
@@ -1,54 +1,61 @@
-window.onload = function() {
-    var findPlace = document.querySelector('#findPlaceForm');
-    var uploadRoom = document.querySelector('#uploadRoomForm');
-    var signUp = document.querySelector('#signUpForm');
-    var logIn = document.querySelector('#logInForm');
-    var signUpBtn = document.querySelector("#signUpBtn");
-   
-    findPlace.onsubmit = findPlaceOnSubmit;
-    uploadRoom.onsubmit = uploadRoomOnSubmit;
-    signUp.onsubmit = signUpOnSubmit;
-    logIn.onsubmit = logInOnSubmit;
-    signUpBtn.onclick = signUpBtnOnClick;
-  };
-  
-  function findPlaceOnSubmit(e) {
-    let form = document.querySelector('#findPlaceForm');
-    form.classList.add('was-validated');
-  
-    if (!form.checkValidity()) {
-      e.preventDefault();
-    }
-  }
+window.onload = function () {
+  var findPlace = document.querySelector("#findPlaceForm");
+  var uploadRoom = document.querySelector("#uploadRoomForm");
+  var signUp = document.querySelector("#signUpForm");
+  var logIn = document.querySelector("#logInForm");
+  var signUpBtn = document.querySelector("#signUpBtn");
 
-  function uploadRoomOnSubmit(e) {
-    let form = document.querySelector('#uploadRoomForm');
-    form.classList.add('was-validated');
-  
-    if (!form.checkValidity()) {
-      e.preventDefault();
-    }
-  }
 
-  function signUpOnSubmit(e) {
-    let form = document.querySelector('#signUpForm');
-    form.classList.add('was-validated');
-  
-    if (!form.checkValidity()) {
-      e.preventDefault();
-    }
-  }
+  findPlace.onsubmit = findPlaceOnSubmit;
+  uploadRoom.onsubmit = uploadRoomOnSubmit;
+  signUp.onsubmit = signUpOnSubmit;
+  logIn.onsubmit = logInOnSubmit;
+  signUpBtn.onclick = signUpBtnOnClick;
+};
 
-  function logInOnSubmit(e) {
-    let form = document.querySelector('#logInForm');
-    form.classList.add('was-validated');
-  
-    if (!form.checkValidity()) {
-      e.preventDefault();
-    }
-  }
+function findPlaceOnSubmit(e) {
+  let form = document.querySelector("#findPlaceForm");
+  form.classList.add("was-validated");
 
-  function signUpBtnOnClick() {
-    $("#logIn").removeClass("fade").modal("hide");
-    $("#signUp").addClass("fade").modal("show");
+  if (!form.checkValidity()) {
+    e.preventDefault();
   }
+}
+
+function uploadRoomOnSubmit(e) {
+  let form = document.querySelector("#uploadRoomForm");
+  form.classList.add("was-validated");
+
+  if (!form.checkValidity()) {
+    e.preventDefault();
+  }
+}
+
+function signUpOnSubmit(e) {
+  let form = document.querySelector("#signUpForm");
+  form.classList.add("was-validated");
+
+  if (!form.checkValidity()) {
+    e.preventDefault();
+  }
+}
+
+function logInOnSubmit(e) {
+  let form = document.querySelector("#logInForm");
+  form.classList.add("was-validated");
+
+  if (!form.checkValidity()) {
+    e.preventDefault();
+  }
+}
+
+function signUpBtnOnClick() {
+  $("#logIn").removeClass("fade").modal("hide");
+  $("#signUp").addClass("fade").modal("show");
+}
+
+//clear forms
+function resetForm(form){
+  // console.log(form);
+  document.getElementById(form).reset();
+}
